8d78872ff241537f8c02e712e074ff0a1f3b832b
JavierE195 <ellerbrockjavier@hotmail.com>
2017-02-20 22:37:29 -0600

    Add files via upload

---
+++ CubeNumber-UsingMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Insert a number: ");
+
+            input = Console.ReadLine();
+            int result = CubeNumber(int.Parse(input));
+
+            Console.Write("The cube of {0} is {1}", input, result);
+            Console.ReadKey();
+
+        }
+
+        public static int CubeNumber( int N)
+        {
+            return N * N * N; 
+        }
+    }
+}
